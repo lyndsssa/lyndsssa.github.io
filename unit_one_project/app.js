ac7dc7f8a7ffd49c44855941b865c2c1d3d0d717
lyndsey ajster <lyndsssa@yahoo.com>
2019-04-17 13:47:58 -0700

    so close

--- unit_one_project/app.js
+++ unit_one_project/app.js
@@ -1,5 +1,38 @@
 $(() => {
 
+  const mainFun = (requestUrl) => {
+
+    const $ul = $('.recpies');
+
+    $.ajax({
+      url: requestUrl,
+      //dataType: "jsonp",
+      //jsonpCallback: "logResults",
+      success: function(data) {
+        console.log(data);
+        //console.log(data.meals[0].strMeal);
+
+        //  $ul.empty();
+        //make loop to cycle through 5 items from data
+        for (let i = 0; i < 5; i++) {
+
+          const recipePic= data.meals[i].strMealThumb;
+
+          console.log(data.meals[i]);  //log 5 meals
+          const $li = $('<li>');
+          console.log($li);
+          //$li.text(data.meals[i].strMeal).attr('id', i);
+          $li.append($('<a>').attr('href', recipePic).text(data.meals[i].strMeal));
+          //made the information from data clickable links, takes you to the photo, my api does not have the actual recipe http site
+          $ul.append($li);
+          console.log($ul);
+
+        }
+        $('#container').append($ul);
+      }
+    })
+  }
+
   const url = 'https://www.themealdb.com/api/json/v1/1/filter.php'
   console.log(url);
   //api call & ingredient search
@@ -12,57 +45,35 @@ $(() => {
     const array = [];
     const $ul = $('.recpies');
     //create array to file through all of the data when user types key word
-$ul.empty();
+    $ul.empty();
     let requestUrl = url + '?a=' + userInput;
-      console.log(requestUrl);
+    console.log(requestUrl);
 
-    $.ajax({
-            url: requestUrl,
-            //dataType: "jsonp",
-            //jsonpCallback: "logResults",
-            success: function(data) {
-              console.log(data);
+    mainFun(requestUrl); //calls new function, running code there
               //console.log(data.meals[0].strMeal);
 
-  //  $ul.empty();
-  //make loop to cycle through 5 items from data
-              for (let i = 0; i < 5; i++) {
-
-                const recipePic= data.meals[i].strMealThumb;
-
-                console.log(data.meals[i]);  //log 5 meals
-                const $li = $('<li>');
-                console.log($li);
-                //$li.text(data.meals[i].strMeal).attr('id', i);
-                $li.append($('<a>').attr('href', recipePic).text(data.meals[i].strMeal));
-                //made the information from data clickable links, takes you to the photo, my api does not have the actual recipe http site
-                $ul.append($li);
-                console.log($ul);
-
-                  }
-                  $('#container').append($ul);
-                }
-            })
-        })
-      })
-
-
-      function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+  })
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    console.log(event.currentTarget);
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    console.log(dropdowns);
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      console.log(event.currentTarget);
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      console.log(dropdowns);
+      mainFun("yay");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
       }
     }
   }
+
+
+})
+// declaring function outside of the ajax because it is already defined in html 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
